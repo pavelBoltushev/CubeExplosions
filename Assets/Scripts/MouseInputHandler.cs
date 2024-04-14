6228d8bc3f88b03f8b2181777c31681d43a07908
pavelBoltushev <pavelboltushev@gmail.com>
2024-04-14 22:26:29 +0300

    init

---
+++ Assets/Scripts/MouseInputHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;    
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnMouseClick();
+    }
+
+    private void OnMouseClick()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.gameObject.TryGetComponent(out Cube clickedCube))
+        {
+            clickedCube.OnClicked();
+        }
+    }
+}
