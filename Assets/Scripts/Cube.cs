9f2f0e84ef648f2324fb9335931038cf67dd3eda
pavelBoltushev <pavelboltushev@gmail.com>
2024-04-15 14:03:58 +0300

    correction

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -10,32 +10,29 @@ public class Cube : MonoBehaviour
 
     public void OnClicked()
     {
-        if(Random.Range(0f, 1f) <= _separationChance)
-        {
-            Separate();
-        }
+        if(Random.Range(0f, 1f) <= _separationChance)        
+            Separate();        
 
         Destroy(gameObject);
     }
 
-    public void ReduceSeparationChanceByHalf()
+    public void Init(Vector3 currentPosition)
     {
         _separationChance /= 2;
-    }
+        transform.localScale /= 2;
+        GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));        
+        GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, currentPosition, 1f);
+    }    
 
     private void Separate()
     {
-        Vector3 currentPosition = transform.position;
-        Vector3 currentScale = transform.localScale;
+        Vector3 currentPosition = transform.position;        
         int newCubesNumber = Random.Range(_minNewCubesNumber, _maxNewCubesNimber + 1);
 
         for (int i = 0; i < newCubesNumber; i++)
         {
-            GameObject newCube = Instantiate(gameObject, currentPosition, Quaternion.identity);
-            newCube.transform.localScale = currentScale / 2;
-            newCube.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-            newCube.GetComponent<Cube>().ReduceSeparationChanceByHalf();
-            newCube.GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, currentPosition, 1f);
+            Cube newCube = Instantiate(this, currentPosition, Quaternion.identity);
+            newCube.Init(currentPosition);
         }
     }
 }
